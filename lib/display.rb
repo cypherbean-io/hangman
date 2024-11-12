251c6507cef52a9b60b33ef9061650c663eb75d2
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-11-11 20:41:25 -0600

    Add ASCII art display

--- lib/display.rb
+++ lib/display.rb
@@ -1,7 +1,69 @@
 # frozen_string_literal: true
 
 class Display
+  # ASCII art for different stages of the hangman
+  HANGMAN_STATES = [
+    "
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    ==========",
+    "
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========="
+  ].freeze
+
   def self.show_game_state(game_state)
+    system('clear') || system('cls')
+    puts HANGMAN_STATES[6 - game_state.remaining_attempts]
     puts "\nWord: #{game_state.masked_word}"
     puts "Remaining attempts: #{game_state.remaining_attempts}"
     puts "Guessed letters: #{game_state.guessed_letters.join(', ')}"
